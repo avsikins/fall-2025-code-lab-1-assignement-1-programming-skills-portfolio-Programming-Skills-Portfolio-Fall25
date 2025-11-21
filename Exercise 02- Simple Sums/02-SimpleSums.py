85ee0e1976219f596b86f16fa234eec37bd5e2c6
Ava Sophia <avsikins@gmail.com>
2025-11-21 22:36:53 +0400

    Create 02-SimpleSums.py

---
+++ Exercise 02- Simple Sums/02-SimpleSums.py
@@ -0,0 +1,8 @@
+# Exercise 02: Simple Sums
+# This program calculates the sum of two numbers and prints the result.
+
+val1 = 8
+val2 = 10
+sum = val1 + val2
+
+print(f"The sum of {val1} and {val2} is {sum}.")
