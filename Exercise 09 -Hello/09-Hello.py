5bf4d691ec6dd562e552eb698bcdd4dcb406878e
Ava Sophia <avsikins@gmail.com>
2025-11-21 22:37:16 +0400

    Create 09-Hello.py

---
+++ Exercise 09 -Hello/09-Hello.py
@@ -0,0 +1,14 @@
+# Exercise 09: Hello
+# This program defines a function that prints "Hello" and calls it from the main function.
+
+# Function to print Hello
+def hello(): 
+    print("Hello")
+
+# Main function to call hello
+def main():
+    hello()
+
+# Call the main function
+if __name__ == "__main__":
+    main() 
