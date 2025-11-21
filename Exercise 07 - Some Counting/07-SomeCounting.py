9e532b8b094f7c86445e02e41562e669d332094a
Ava Sophia <avsikins@gmail.com>
2025-11-21 22:37:11 +0400

    Create 07-SomeCounting.py

---
+++ Exercise 07 - Some Counting/07-SomeCounting.py
@@ -0,0 +1,32 @@
+# Exercise 07: Some Counting
+# This program performs various counting tasks using for loops.
+
+# First loop: Count from 0 to 49
+for a in range(50):
+    print(a)
+else:
+    print()
+
+# Second loop: Count from 50 down to 0
+for b in range(50, -1, -1):
+    print(b)
+else:
+    print()
+
+# Third loop: Count from 30 to 49
+for c in range(30, 50):
+    print(c)
+else:
+    print()
+
+# Fourth loop: Count from 50 down to 11, stepping by 2
+for d in range(50, 10, -2):
+    print(d)
+else:
+    print()
+
+# Fifth loop: Count from 100 to 199, stepping by 5
+for e in range(100, 200, 5):
+    print(e)
+else:
+    print()
